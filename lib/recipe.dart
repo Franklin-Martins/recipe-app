7191d9e5770cf3ce7c8d019656754f168fb6b411
Franklinxd <gentilezaxd@gmail.com>
2022-01-15 16:49:07 -0400

    Add Slider component to add interactivity

--- lib/recipe.dart
+++ lib/recipe.dart
@@ -1,14 +1,67 @@
 class Recipe{
   String label;
   String imageUrl;
+  int servings;
 
-  Recipe(this.label, this.imageUrl);
+  List<Ingredient> ingredients;
+
+  Recipe(this.label, this.imageUrl, this.servings, this.ingredients);
 
   static List<Recipe> samples = [
-    Recipe('Spaghetti','assets/spaghetti.jpg'),
-    Recipe('Spaghetti','assets/spaghetti.jpg'),
-    Recipe('Spaghetti','assets/spaghetti.jpg'),
-    Recipe('Spaghetti','assets/spaghetti.jpg'),
-    Recipe('Spaghetti','assets/spaghetti.jpg'),
+    Recipe(
+      'Spaghetti',
+      'assets/spaghetti.jpg', 
+      4, 
+      [
+        Ingredient(1, 'box', 'Spaghetti'),
+        Ingredient(4, '', 'Frozen Meat Balls'),
+        Ingredient(0.5, 'jar', 'sauces'),
+      ]
+    ),
+    Recipe(
+      'Spaghetti',
+      'assets/spaghetti.jpg', 
+      4, 
+      [
+        Ingredient(1, 'box', 'Spaghetti'),
+        Ingredient(4, '', 'Frozen Meat Balls'),
+        Ingredient(0.5, 'jar', 'sauces'),
+      ]
+    ),Recipe(
+      'Spaghetti',
+      'assets/spaghetti.jpg', 
+      4, 
+      [
+        Ingredient(1, 'box', 'Spaghetti'),
+        Ingredient(4, '', 'Frozen Meat Balls'),
+        Ingredient(0.5, 'jar', 'sauces'),
+      ]
+    ),Recipe(
+      'Spaghetti',
+      'assets/spaghetti.jpg', 
+      4, 
+      [
+        Ingredient(1, 'box', 'Spaghetti'),
+        Ingredient(4, '', 'Frozen Meat Balls'),
+        Ingredient(0.5, 'jar', 'sauces'),
+      ]
+    ),Recipe(
+      'Spaghetti',
+      'assets/spaghetti.jpg', 
+      4, 
+      [
+        Ingredient(1, 'box', 'Spaghetti'),
+        Ingredient(4, '', 'Frozen Meat Balls'),
+        Ingredient(0.5, 'jar', 'sauces'),
+      ]
+    ),
   ];
+}
+
+class Ingredient{
+  double quantity;
+  String measure;
+  String name;
+
+  Ingredient(this.quantity, this.measure, this.name);
 }
